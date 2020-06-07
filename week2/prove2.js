1b4dc815aba75c1c5d1873e4072c38c477aa45c7
Justin Paton <pat16012@byui.edu>
2020-06-06 20:26:27 -0600

    Finished Team Activity Week 2

--- week2/prove2.js
+++ week2/prove2.js
@@ -1,5 +1,33 @@
-const userInput = document.getElementsByName("input");
+// Get user input and put it on the blank div when button clicked
 function addItem() {
-    document.getElementById("update").innerHTML = userInput.toString();
+    document.getElementById("update").innerHTML = document.getElementById("input").value;
+}
+let y =  [];
+
+
+function sumNum() {
+    let userNum = 0;
+   userNum = Number(document.getElementById("numInput").value);
+   if (isNaN(userNum)) {
+      alert('Please Enter a Number!')
+   }
+   else {
+       y.push(userNum);
+       document.getElementById("summedNumber").innerHTML = y.reduce((total, amount) => total + amount);
+   }
+
+
 
 }
+
+function addInputs() {
+let num1 = Number(document.getElementById("num1").value);
+let num2 = Number(document.getElementById("num2").value);
+document.getElementById("numbersAdded").innerHTML = String(num1 + num2);
+}
+
+
+
+
+
+
